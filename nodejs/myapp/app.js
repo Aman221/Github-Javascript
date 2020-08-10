987ea2445e9b97fa8186c65b68c36ecf550d502a
Amanuel Ayalew <amanu.ayalew@gmail.com>
2020-08-09 18:48:56 -0700

    jsonParser addition

--- nodejs/myapp/app.js
+++ nodejs/myapp/app.js
@@ -1,13 +1,14 @@
 // imports
 const express = require('express'),
+  bodyParser = require('body-parser'),
   app = express(),
   taskService = require("./Source/Services/TaskService"),
   hostname = '127.0.0.1',
   port = 3000;
 
-// app.use((express.json)),
+app.use(bodyParser.json({extender: true, limit: "20mb"}));
 
-app.get("/", (req, res, done)=> taskService.sayHello);
+app.get("/", taskService.sayHello);
 app.get("/getTasks", taskService.getTasks);
 app.get("/getMyProfile", taskService.getMyProfile);
 
